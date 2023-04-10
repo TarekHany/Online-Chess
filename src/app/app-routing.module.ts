bbdc36441f470b866778c3e26fd29736c38a935d
Tarek Hany <tata.hany37@gmail.com>
2023-04-10 22:43:38 +0200

    Re-initiated Angular template with Angular version 12.0.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ChessBoardComponent } from './chess-board/chess-board.component';
+import { MainPageComponent } from './main-page/main-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'mainpage', component: MainPageComponent },
+  { path: 'iframe', component: ChessBoardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ChessBoardComponent } from './chess-board/chess-board.component';
+// import { NgxChessBoardModule } from "ngx-chess-board";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ChessBoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // NgxChessBoardModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
